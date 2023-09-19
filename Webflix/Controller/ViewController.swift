3063f66ab345dcfcb9f85abebdebf13cafb897dc
Kevsl <ktr2@pm.me>
2023-09-19 11:15:38 +0200

    first commit

---
+++ Webflix/Controller/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Webflix
+//
+//  Created by K on 19/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchField: UITextField!
+    
+    var searchValue = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+        view.addGestureRecognizer(UIGestureRecognizer(target:self, action:#selector(hideKeyboard)))    }
+    
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
+    
+
+    @IBAction func searchFIeldAction(_ sender: UITextField) {
+        searchValue = searchField.text ?? ""
+        getMovies()
+    }
+    
+    
+    
+    
+    func getMovies(){
+        let url = "https://www.omdbapi.com/?s=\(searchValue)&apikey=c6c2a5e9"
+        
+        print(url)
+    }
+}
+
+
+
+extension ViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
---
+++ Webflix/Model/Movie.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+    struct Movie: Codable {
+        let title: String
+        let year: String
+        let poster: String
+        let imdbID:String
+    }
+
+
+
